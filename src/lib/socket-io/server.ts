6f58470aba322f906f5185ec3846cdc0879dd3a7
ushu3323 <redspower10@gmail.com>
2024-03-19 20:40:56 -0300

    Serve frontend static files in production

--- src/lib/socket-io/server.ts
+++ src/lib/socket-io/server.ts
@@ -1,5 +1,27 @@
 import { Server, type ServerOptions } from "socket.io";
-import type { Server as HttpServer } from 'http';
+import type { Server as HttpServer } from "http";
 import { ClientToServerEvents, ServerToClientEvents } from "../../types/socket";
 
-export const createServer = (server?: HttpServer | undefined, opts?: Partial<ServerOptions>): Server<ClientToServerEvents, ServerToClientEvents> => new Server<ClientToServerEvents, ServerToClientEvents>(server, opts)
+export const createServer = (
+  server?: HttpServer | undefined,
+  opts?: Partial<ServerOptions>
+): Server<ClientToServerEvents, ServerToClientEvents> => {
+  const io = new Server<ClientToServerEvents, ServerToClientEvents>(
+    server,
+    opts
+  );
+
+  io.on("connection", (socket) => {
+    socket.broadcast.emit("userJoined", socket.id);
+
+    socket.on("message", (payload, callback) => {
+      socket.broadcast.emit("message", {
+        ...payload,
+        author_id: socket.id,
+      });
+      callback({ status: "OK" });
+    });
+  });
+
+  return io;
+};
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,54 +1,69 @@
 import express from "express";
 import { createServer as createSocketServer } from "../lib/socket-io/server";
-import fs from 'fs'
-import path from 'path'
-import { createServer as createViteServer } from 'vite'
+import fs from "fs/promises";
+import path from "path";
+import type { ViteDevServer } from "vite";
+
+const isProduction = process.env.NODE_ENV === "production";
+const port = process.env.PORT || 5173;
+const base = process.env.BASE || "/";
+
+// Cached production assets
+const templateHtml = isProduction
+  ? await fs.readFile("./dist/index.html", "utf-8")
+  : "";
 
 async function createServer() {
-  const app = express()
+  const app = express();
   const io = createSocketServer();
 
-  io.on('connection', (socket) => {
-    socket.broadcast.emit("userJoined", socket.id);
-
-    socket.on("message", (payload, callback) => {
-      socket.broadcast.emit("message", {
-        ...payload,
-        author_id: socket.id,
-      })
-      callback({status: "OK"})
-    })
-  })
-
   app.get("/api", (_req, res) => {
-    res.send("Hello!")
-  })
+    res.send("Hello!");
+  });
 
-  const vite = await createViteServer({
-    server: { middlewareMode: true },
-    appType: 'custom'
-  })
+  let vite: ViteDevServer | undefined;
+  if (isProduction) {
+    const compression = (await import("compression")).default;
+    const sirv = (await import("sirv")).default;
+    app.use(compression());
+    app.use(base, sirv("./dist/", { extensions: [] }));
+  } else {
+    const { createServer: createViteServer } = await import("vite");
+    vite = await createViteServer({
+      server: { middlewareMode: true },
+      appType: "custom",
+      base,
+    });
+    app.use(vite.middlewares);
+  }
 
-  app.use(vite.middlewares)
-  app.use('*', async (req, res, next) => {
-    const url = req.originalUrl
-
+  app.use("*", async (req, res) => {
     try {
-      let template = fs.readFileSync(
-        path.resolve(process.cwd(), 'index.html'),
-        'utf-8',
-      )
-      template = await vite.transformIndexHtml(url, template)
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(template)
-    } catch (e) {
-      vite.ssrFixStacktrace(e as Error)
-      next(e)
-    }
-  })
+      const url = req.originalUrl.replace(base, "");
 
-  const port = +(process.env.PORT || 3000);
-  const server = app.listen(port, () => console.log("Server is listening at port", port))
+      let template: string;
+      if (isProduction) {
+        template = templateHtml;
+      } else {
+        template = await fs.readFile(
+          path.resolve(process.cwd(), "index.html"),
+          "utf-8"
+        );
+        template = await vite!.transformIndexHtml(url, template);
+      }
+
+      res.status(200).set({ "Content-Type": "text/html" }).end(template);
+    } catch (e) {
+      const error = e as Error;
+      vite?.ssrFixStacktrace(error);
+      res.status(500).end(error.stack);
+    }
+  });
+
+  const server = app.listen(port, () =>
+    console.log("Server is listening at port", port)
+  );
   io.attach(server);
 }
 
-createServer()
+createServer();
