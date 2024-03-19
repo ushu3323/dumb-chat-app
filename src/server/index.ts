dcdb23fde9dcc7fe2181ac3fa2173784bcfec01c
ushu3323 <redspower10@gmail.com>
2024-03-19 12:29:58 -0300

    Run vite as a middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,29 +1,54 @@
 import express from "express";
-import { Server } from "socket.io";
-import type { ClientToServerEvents, ServerToClientEvents } from "../types/socket";
+import { createServer as createSocketServer } from "../lib/socket-io/server";
+import fs from 'fs'
+import path from 'path'
+import { createServer as createViteServer } from 'vite'
 
-export const app = express();
-export const io: Server<ClientToServerEvents, ServerToClientEvents> = new Server()
+async function createServer() {
+  const app = express()
+  const io = createSocketServer();
 
-io.on('connection', (socket) => {
-  socket.broadcast.emit("userJoined", socket.id);
+  io.on('connection', (socket) => {
+    socket.broadcast.emit("userJoined", socket.id);
 
-  socket.on("message", (payload, callback) => {
-    socket.broadcast.emit("message", {
-      ...payload,
-      author_id: socket.id,
+    socket.on("message", (payload, callback) => {
+      socket.broadcast.emit("message", {
+        ...payload,
+        author_id: socket.id,
+      })
+      callback({status: "OK"})
     })
-    callback({status: "OK"})
   })
-})
 
-app.get("/api", (_req, res) => {
-  res.send("Hello!")
-})
+  app.get("/api", (_req, res) => {
+    res.send("Hello!")
+  })
 
-if (process.env.NODE_ENV !== "development") {
-  // Vite server is not running, we need to run a server ourselves
-  const port = process.env.PORT || 3000;
-  const server = app.listen(port, () => console.log("Listening at port", port));
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom'
+  })
+
+  app.use(vite.middlewares)
+  app.use('*', async (req, res, next) => {
+    const url = req.originalUrl
+
+    try {
+      let template = fs.readFileSync(
+        path.resolve(process.cwd(), 'index.html'),
+        'utf-8',
+      )
+      template = await vite.transformIndexHtml(url, template)
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(template)
+    } catch (e) {
+      vite.ssrFixStacktrace(e as Error)
+      next(e)
+    }
+  })
+
+  const port = +(process.env.PORT || 3000);
+  const server = app.listen(port, () => console.log("Server is listening at port", port))
   io.attach(server);
 }
+
+createServer()
